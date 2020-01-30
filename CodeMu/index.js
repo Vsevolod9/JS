82118525e6d354147abd7c6a1aa3b0031f54ada2
IMNAP <leto99999@gmail.com>
2020-01-30 13:26:39 +0300

    do 39 lesson

--- CodeMu/index.js
+++ CodeMu/index.js
@@ -1,5 +1,38 @@
 'use strict';
 
+let arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+let day = arr[4];
+
+for ( let i = 0; i < arr.length; i++) {
+
+    if ( i < 4) {
+        document.write(arr[i] + ' ');
+    }
+    
+    if ( i > 4) {
+        document.write(arr[i] + ' ');
+    }
+    
+    if ( i == 5) {
+        document.write('<i> ' + day + ' </i>');
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i < arr.length; i++) {
+
+//     document.write(arr[i] + '<p>');
+// };
 
 
 
