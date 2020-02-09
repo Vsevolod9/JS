dd9b41bd77a28cf765f8440f1548b736872f472c
IMNAP <leto99999@gmail.com>
2020-02-09 19:06:01 +0300

    do lesson 75 Yaahooo

--- CodeMu/index.js
+++ CodeMu/index.js
@@ -1,40 +1,57 @@
-"use strict";
+let users = [{
+        name: 'name1',
+        active: true,
+    },
+    {
+        name: 'name2',
+        active: false,
+    },
+    {
+        name: 'name3',
+        active: true,
+    },
+    {
+        name: 'name4',
+        active: false,
+    },
+    {
+        name: 'name5',
+        active: true,
+    },
+];
 
-let students = {
-	'group1' : {
-		'subgroup11': ['student111', 'student112', 'student113'],
-		'subgroup12': ['student121', 'student122', 'student123'],
-	},
-	'group2' : {
-		'subgroup21': ['student211', 'student212', 'student213'],
-		'subgroup22': ['student221', 'student222', 'student223'],
-	},
-	'group3' : {
-		'subgroup31': ['student311', 'student312', 'student313'],
-		'subgroup32': ['student321', 'student322', 'student323'],
-	},
-};
 
-addStudent('group3', 'subgroup31', 'Vasia' );
-addStudent('group4');
+let newArr = [];
 
+changeValue(true, 1, users);
 
-console.log(students);
+function changeValue(value, userNumber, nameOfArr) {
 
-function addStudent(group, subgroup, newStudent) {
+    nameOfArr[userNumber].active = value;
 
-    if ( students[group] === undefined) {
-        students[group] = {};
-    }   
-    students[group].push(group);
-
-      students[group][subgroup].push(newStudent);
-    
+    for (let person of users) {
+        newArr.push(person);
+    }
 }
 
+console.log('Это новый массив: ', newArr);
+
+
+// for (let card of users) {
 
 
 
+//     }
+
+// console.log(users);
 
 
 
+// if (card.active)  { 
+
+//     card.active = false;
+
+// } else {
+
+//     card.active = true;
+// }
