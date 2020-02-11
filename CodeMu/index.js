100a7b3540cccd69732554d039b034e3569b018e
IMNAP <leto99999@gmail.com>
2020-02-11 18:30:03 +0300

    lesson 76 problem 8

--- CodeMu/index.js
+++ CodeMu/index.js
@@ -1,19 +1,31 @@
 'use strict';
 
 
-let num = 587;
+let arrWhile = [];
+let arrFor = [];
 
-let sqare = Math.sqrt(num);
+let i = 1;
 
-let a  = Math.ceil(sqare);
-let b = Math.floor(sqare);  
+function getRandomInt(min, max) {
+    
+    
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-let obj = {};
+while ( i <= 10) {
+    
+    arrWhile.push(getRandomInt(10, 100));
+    i++; 
+}
 
-obj['ceil'] = a; 
-obj.floor = b; 
+for (let i = 1; i <=10; i++ ) {
+
+    arrFor.push(getRandomInt(10, 100));
+}
 
 
-console.log(obj);
+
+console.log(arrWhile);
+console.log(arrFor);
 
     
