5834436c93173dd20655b9bd1aae650619c2e019
IMNAP <leto99999@gmail.com>
2020-02-12 15:18:06 +0300

    lesson 79

--- CodeMu/index.js
+++ CodeMu/index.js
@@ -1,31 +1,112 @@
 'use strict';
 
+ let arr = [1, 2, 3, 4, 5];
 
-let arrWhile = [];
-let arrFor = [];
 
-let i = 1;
+ arr.indexOf(3);
 
-function getRandomInt(min, max) {
-    
-    
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
-while ( i <= 10) {
-    
-    arrWhile.push(getRandomInt(10, 100));
-    i++; 
-}
+console.log(arr.indexOf(3, 2));
 
-for (let i = 1; i <=10; i++ ) {
+// 10 -----------
 
-    arrFor.push(getRandomInt(10, 100));
-}
+
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.splice(1, 0, 'a', 'b' );
+// arr.splice(5, 0, 'c' );
+// arr.splice(8, 0, 'e' );
 
 
 
-console.log(arrWhile);
-console.log(arrFor);
 
-    
+// console.log(arr);
+
+
+// 9 ----------------------------
+
+
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.splice(3, 0, 'a', 'b', 'c' );
+
+// console.log(arr);
+
+
+
+// 7 ----------------------------
+
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.splice(2, 0, 2, 3, 4);
+
+// console.log(arr);
+
+
+
+
+
+
+// 6 ----------------------------
+
+
+// let arr = [1, 2, 3, 4, 5];
+// let sub = arr.slice(3, 6);
+
+// console.log(sub);
+
+
+// 5 ----------------------------
+
+
+// let arr = [1, 2, 3, 4, 5];
+// let sub = arr.slice(0, 3);
+
+// console.log(sub);
+
+
+// 4 ----------------------------
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// let sub = arr.pop();
+
+
+// console.log(arr);
+// console.log(sub);
+
+
+
+
+
+
+// 3  -------------------------
+
+// let arr = ['js', 'css', 'jq'];
+
+// let elem = arr.shift();
+
+// console.log(arr);
+// console.log(elem);
+
+
+
+
+
+// 2  -------------------------
+
+// let arr = [1, 2, 3];
+
+// let b = arr.unshift(4, 5, 6);
+
+// console.log(arr);
+// console.log(b);
+
+
+
+// 1  -------------------------
+
+// let arr = [1, 2, 3];
+
+// arr.push(4, 5, 6);
+
+// console.log(arr);
