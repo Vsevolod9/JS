51fdbcf53333eae272f795f7e7d9f50e875da7e6
IMNAP <leto99999@gmail.com>
2020-02-12 20:56:35 +0300

    lesson 80

--- CodeMu/index.js
+++ CodeMu/index.js
@@ -1,112 +1,33 @@
 'use strict';
 
- let arr = [1, 2, 3, 4, 5];
+let str = 'abc def';
 
+let result = str.split(' ').reverse().join(' ');
 
- arr.indexOf(3);
+console.log(result); // выведет '54321'
 
 
-console.log(arr.indexOf(3, 2));
 
-// 10 -----------
 
 
-// let arr = [1, 2, 3, 4, 5];
 
-// arr.splice(1, 0, 'a', 'b' );
-// arr.splice(5, 0, 'c' );
-// arr.splice(8, 0, 'e' );
 
 
 
 
-// console.log(arr);
 
+//---------------------
 
-// 9 ----------------------------
 
+// let str1 = 'var_test_text';
 
-// let arr = [1, 2, 3, 4, 5];
+// let words1 = str1.split('_');
 
-// arr.splice(3, 0, 'a', 'b', 'c' );
+// for (let i = 1; i < words1.length; i++) {
+	
+// 	words1[i] = words1[i].slice(0, 1).toUpperCase() + words1[i].slice(1);
+// }
+// let result1 = words1.join('');
+// console.log(result1);
 
-// console.log(arr);
 
-
-
-// 7 ----------------------------
-
-// let arr = [1, 2, 3, 4, 5];
-
-// arr.splice(2, 0, 2, 3, 4);
-
-// console.log(arr);
-
-
-
-
-
-
-// 6 ----------------------------
-
-
-// let arr = [1, 2, 3, 4, 5];
-// let sub = arr.slice(3, 6);
-
-// console.log(sub);
-
-
-// 5 ----------------------------
-
-
-// let arr = [1, 2, 3, 4, 5];
-// let sub = arr.slice(0, 3);
-
-// console.log(sub);
-
-
-// 4 ----------------------------
-
-// let arr = ['a', 'b', 'c', 'd', 'e'];
-// let sub = arr.pop();
-
-
-// console.log(arr);
-// console.log(sub);
-
-
-
-
-
-
-// 3  -------------------------
-
-// let arr = ['js', 'css', 'jq'];
-
-// let elem = arr.shift();
-
-// console.log(arr);
-// console.log(elem);
-
-
-
-
-
-// 2  -------------------------
-
-// let arr = [1, 2, 3];
-
-// let b = arr.unshift(4, 5, 6);
-
-// console.log(arr);
-// console.log(b);
-
-
-
-// 1  -------------------------
-
-// let arr = [1, 2, 3];
-
-// arr.push(4, 5, 6);
-
-// console.log(arr);
